7849cc57d1b094456bb42ee60ce8481490a8af45
Phanidra Mangipudi <Phanidra.Mangipudi@sunpowercorp.com>
2020-02-26 18:22:21 -0800

    added loading image

--- src/services/firbase.service.ts
+++ src/services/firbase.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as data from '../data.json';
 
 @Injectable({
   providedIn: 'root'
